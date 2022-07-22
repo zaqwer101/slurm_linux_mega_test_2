31faf65d75d1348757e0424149dceb7514ab71c5
zaqwer101 <platonov256@gmail.com>
2022-07-22 09:58:32 +0300

    fix

--- test.sh
+++ test.sh
@@ -5,7 +5,7 @@ echo "1.1: $STATUS11"
 [[ $(cat /var/log/signal.log 2>/dev/null | tr "\n" ":") == '11:2:15:14:' ]] && STATUS12=Success
 echo "1.2: $STATUS12"
 
-TEST1="$(cat /tmp/simple_process 2>/dev/null | tr -d "\n" | tr -d ' ')"; TEST2="$(cat /proc/$(ps aux | grep simple_process | grep -v 'grep' | awk '{print $2}')/environ 2>/dev/null | strings | grep TEST | awk -F'=' '{print $2}' | tr -d '\n')"; [[ $TEST1 == $TEST2 ]] && STATUS13=Success
+TEST1="$(cat /tmp/simple_process 2>/dev/null | tr -d "\n" | tr -d ' ')"; TEST2="$(cat /proc/$(ps aux | grep simple_process | grep -v 'grep' | awk '{print $2}')/environ 2>/dev/null | strings | grep TEST | awk -F'=' '{print $2}' | tr -d '\n')"; [[ $TEST1 == $TEST2 && $TEST1 != "" ]] && STATUS13=Success
 echo "1.3: $STATUS13"
 
 [[ $(su test -c "source ~/.bashrc && source ~/.bash_profile && umask" 2>/dev/null) == "0077" ]] && STATUS21=Success
