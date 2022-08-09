f10e520184eb71a723d0042be126b0d57856a37d
zaqwer101 <platonov256@gmail.com>
2022-08-09 10:36:42 +0300

    fix

--- test.sh
+++ test.sh
@@ -8,11 +8,11 @@ echo "1.2: $STATUS12"
 TEST1="$(sudo cat /tmp/simple_process 2>/dev/null | tr -d "\n" | tr -d ' ')"; TEST2="$(sudo cat /proc/$(ps aux | grep simple_process | grep -v 'grep' | awk '{print $2}')/environ 2>/dev/null | strings | grep TEST | awk -F'=' '{print $2}' | tr -d '\n')"; [[ $TEST1 == $TEST2 && $TEST1 != "" ]] && STATUS13=Success
 echo "1.3: $STATUS13"
 
-[[ $(sudo su test -c "source /etc/profile && umask" 2>/dev/null) == "0077" ]] && STATUS21=Success
+sudo test ! -f /opt/file && STATUS21=Success
 echo "2.1: $STATUS21"
 
 sudo getfacl /opt/house 2>/dev/null | grep "user:tom:rwx" >/dev/null && getfacl /opt/house 2>/dev/null | grep "user:jerry:rwx" >/dev/null && getfacl /opt/house/room 2>/dev/null | grep "user:tom:rwx" >/dev/null && getfacl /opt/house/room 2>/dev/null | grep "user:jerry:rwx" >/dev/null && getfacl /opt/house/kitchen 2>/dev/null | grep "user:tom:rwx" >/dev/null && getfacl /opt/house/kitchen 2>/dev/null | grep "user:jerry:r-x" >/dev/null && getfacl /opt/house/kitchen/cheese 2>/dev/null | grep "user:jerry:r--" >/dev/null && getfacl /opt/house/kitchen/cheese 2>/dev/null | grep "user:tom:-w-" >/dev/null && STATUS22=Success
 echo "2.2: $STATUS22"
 
-sudo test ! -f /opt/file && STATUS23=Success
+[[ $(sudo su test -c "source /etc/profile && umask" 2>/dev/null) == "0077" ]] && STATUS23=Success
 echo "2.3: $STATUS23"
